5d9c46bb668fa4f45c43fdd6affba6508af382c4
GokoshiJr <julioagonzalez18@gmail.com>
2020-11-15 22:00:18 -0400

    Creando la tabla authors

--- crear_tabla_books.sql
+++ crear_tabla_books.sql
@@ -6,13 +6,13 @@
 CREATE TABLE IF NOT EXISTS books (
   /* entero, llave primaria, autoincremental cuando vamos agregando mas elementos */
   /* unsigned para que no guarde el signo del numero entero, seria como el valor absoluto */
-  book_id INTEGER UNSIGNED PRIMARY AUTO_INCREMENT,
-  author,
+  book_id INTEGER UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+  author_id INTEGER UNSIGNED,
   /* para que guarde un texto varchar(num) le indicamos el numero de caracteres */
   /* NOT NULL para que no guarde libros que no tengan titulo */
   title VARCHAR(100) NOT NULL,
   /* DEFAULT es que si no se envia nada pone un valor por defecto*/
-  year_ INTEGER UNSIGNED NOT NULL DEFAULT 1900,
+  `year` INTEGER UNSIGNED NOT NULL DEFAULT 1900,
   /* COMMENT, comentario a la columna, que solo ven los que vean la estructura de la bd */
   languaje VARCHAR(2) NOT NULL DEFAULT 'es' COMMENT 'ISO 639-1 Languaje',
   cover_url VARCHAR(500),
@@ -24,5 +24,10 @@ CREATE TABLE IF NOT EXISTS books (
   sellabe TINYINT(1) DEFAULT 1,
   copies INTEGER NOT NULL DEFAULT 1,
   /* permite guardar grandes cantidades de texto */
-  description_ TEXT
+  `description` TEXT
 );
+
+/* 
+  Si el nombre de una columna coincide con una palabra reservada
+  el nombre lo escribimos en con comillas invertidas `integer` (muy raro)
+*/
